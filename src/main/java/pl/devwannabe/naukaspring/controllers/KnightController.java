71f3016afcf29d043b495ec763fb67431612aad6
Marek <looppega@gmail.com>
2018-08-13 11:49:55 +0200

    web scopes

--- src/main/java/pl/devwannabe/naukaspring/controllers/KnightController.java
+++ src/main/java/pl/devwannabe/naukaspring/controllers/KnightController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import pl.devwannabe.naukaspring.components.TimeComponent;
 import pl.devwannabe.naukaspring.domain.Knight;
+import pl.devwannabe.naukaspring.domain.PlayerInformation;
 import pl.devwannabe.naukaspring.services.KnightService;
 
 import java.util.List;
@@ -16,12 +18,20 @@ import java.util.List;
 public class KnightController {
 
     @Autowired
+    TimeComponent timeComponent;
+
+    @Autowired
+    PlayerInformation playerInformation;
+
+    @Autowired
     KnightService knightService;
 
     @RequestMapping("/knights")
     public String getKnights(Model model) {
         List<Knight> allKnigts = knightService.getAllknights();
         model.addAttribute("knights", allKnigts);
+        model.addAttribute("timecomponent", timeComponent);
+        model.addAttribute("playerInformation", playerInformation);
         return "knights";
     }
 
@@ -29,12 +39,16 @@ public class KnightController {
     public String getKnight(@RequestParam("id") Integer id, Model model) {
         Knight knight = knightService.getKnight(id);
         model.addAttribute("knight", knight);
+        model.addAttribute("timecomponent", timeComponent);
+        model.addAttribute("playerInformation", playerInformation);
         return "knight";
     }
 
     @RequestMapping("/newknight")
     public String createKnight(Model model) {
         model.addAttribute("knight", new Knight());
+        model.addAttribute("timecomponent", timeComponent);
+        model.addAttribute("playerInformation", playerInformation);
         return "knightform";
     }
 
---
+++ src/main/java/pl/devwannabe/naukaspring/domain/PlayerInformation.java
@@ -0,0 +1,21 @@
+package pl.devwannabe.naukaspring.domain;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(value = "session", proxyMode = ScopedProxyMode.TARGET_CLASS)
+public class PlayerInformation {
+
+    public int getGold() {
+        return gold;
+    }
+
+    public void setGold(int gold) {
+        this.gold = gold;
+    }
+
+    private int gold;
+
+}
